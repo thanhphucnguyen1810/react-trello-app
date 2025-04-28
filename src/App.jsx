fb103670269fa5b36c59312fd4c0eb684d6b2193
Nguyen Thanh Phuc <thanhphucnguyen54@gmail.com>
2025-04-28 09:48:41 +0700

    Theming - Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Button } from '@mui/material'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <h1>Hello World</h1>
+      <Typography variant='body2' color="text.secondary">
+        This is a sample text
+      </Typography>
+
       <Button variant="contained">Hello World</Button>
       <Button variant="outlined">Hello World</Button>
       <Button variant="text">Hello World</Button>
