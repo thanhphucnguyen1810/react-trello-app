7a5fad082183358edbb18270ef3f318e209f78f9
Nguyen Thanh Phuc <thanhphucnguyen54@gmail.com>
2025-05-02 09:41:46 +0700

    Add: Board Bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -24,13 +24,14 @@ import CloseIcon from '@mui/icons-material/Close'
 function AppBar() {
   const [search, setSearch] = useState('')
   return (
-    <Box px={2} sx={{
+    <Box sx={{
       width: '100%',
       height: (theme) => theme.trello.AppBarHeight,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
       gap: 2,
+      paddingX: 2,
       overflowX: 'auto',
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0')
     }}>
--- src/theme.js
+++ src/theme.js
@@ -31,11 +31,11 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb': {
-            BorderRadius: '8px',
-            backgroundColor: '#bdc3c7'
+            backgroundColor: '#dcdde1',
+            BorderRadius: '8px'
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: 'rgba(0, 0, 0, 0.4)'
+            backgroundColor: 'white'
           }
         }
       }
@@ -45,36 +45,24 @@ const theme = extendTheme({
         // Name of the slot
         root: {
           // Some CSS
-          textTransform: 'none'
+          textTransform: 'none',
+          borderWidth: '0.5px',
+          '&:hover': { borderWidth: '0.5px' }
         }
       }
     },
     MuiInputLabel: {
       styleOverrides: {
-        root: ( { theme } ) => ({
-          // color: theme.palette.primary.main,
-          fontSize: '0.875rem'
-        })
+        root: { fontSize: '0.875rem'}
       }
     },
     MuiOutlinedInput: {
       styleOverrides: {
-        root: ( { theme } ) => {
-          return {
-            // color: theme.palette.primary.main,
-            fontSize: '0.875rem',
-            // '.MuiOutlinedInput-notchedOutline': {
-            //   borderColor: theme.palette.primary.light
-            // },
-            // '&:hover': {
-            //   '.MuiOutlinedInput-notchedOutline': {
-            //     borderColor: theme.palette.primary.main
-            //   }
-            // },
-            '& fieldset': { borderWidth: '0.5px !important' },
-            '&:hover fieldset': { borderWidth: '1px !important' },
-            '&.Mui-focused fieldset': { borderWidth: '1px !important' }
-          }
+        root: {
+          fontSize: '0.875rem',
+          '& fieldset': { borderWidth: '0.5px !important' },
+          '&:hover fieldset': { borderWidth: '1px !important' },
+          '&.Mui-focused fieldset': { borderWidth: '1px !important' }
         }
       }
     }
