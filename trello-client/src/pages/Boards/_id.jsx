f9316789acc07ecfa5a97b552eac8caca7ef3bde
Nguyen Thanh Phuc <thanhphucnguyen54@gmail.com>
2025-05-22 20:18:43 +0700

    feat: Add API Get Details Of Board

--- trello-client/src/pages/Boards/_id.jsx
+++ trello-client/src/pages/Boards/_id.jsx
@@ -1,17 +1,32 @@
 // Board details
+import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
+
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    // sử dụng react-router-dom để lấy boardId từ URL về
+    const boardId = '682d5ee0378f65e221da0dbc'
+
+    // call API
+    fetchBoardDetailsAPI(boardId)
+      .then((board) => {
+        setBoard(board)
+      })
+  }, [])
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
--- trello-server/src/utils/constants.js
+++ trello-server/src/utils/constants.js
@@ -1,1 +1,7 @@
-// example constants
+
+// NHỮNG DOMAIN ĐƯỢC PHÉP TRUY CẬP TỚI TÀI NGUYÊN CỦA SERVER
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  // sau này sẽ deploy lên domain chính thức
+  // 'https://trello-app.netlify.app': VD
+]
