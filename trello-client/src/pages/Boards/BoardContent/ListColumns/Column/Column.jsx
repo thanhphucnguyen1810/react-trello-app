b383249161da8996534e2d0547b678b6fe1ec9ea
thanhphucnguyen1810 <thanhphucnguyen54@gmail.com>
2025-06-12 10:04:27 +0700

    feat: Update UI UX

--- trello-client/src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ trello-client/src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -20,7 +20,8 @@ import ListCards from './ListCards/ListCards'
 import { mapOrder } from '~/utils/sorts'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-
+import TextField from '@mui/material/TextField'
+import CloseIcon from '@mui/icons-material/Close'
 
 function Column({ column }) {
 
@@ -49,6 +50,24 @@ function Column({ column }) {
 
   // sort cards
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
+
+  const [openNewCardForm, setOpenNewCardForm] = useState(false)
+  const toggleOpenNewCardForm = () => setOpenNewCardForm(!openNewCardForm)
+
+  const [newCardTitle, setNewCardTitle] = useState('')
+  const addNewCard = () => {
+    if (!newCardTitle) {
+      console.error('Please enter Card Title!')
+      return
+    }
+    console.log(newCardTitle)
+
+    // Gọi API ở đây...
+
+    // Đóng trạng thái thêm Card mới và Clear Input
+    toggleOpenNewCardForm()
+    setNewCardTitle('')
+  }
 
   return (
     // Phải bọc div vì vấn đề chiều cao của column khi kéo thả sẽ có bug kiểu flickering;
@@ -138,15 +157,98 @@ function Column({ column }) {
         {/* Box column Footer */}
         <Box sx={{
           height: (theme) => theme.trello.ColumnFooterHeight,
-          p: 2,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between'
+          p: 2
         }}>
-          <Button startIcon={<AddCardIcon />}> Add new card </Button>
-          <Tooltip title="Drag to move">
-            <DragHandleIcon sx={{ cursor: 'pointer' }} />
-          </Tooltip>
+          {!openNewCardForm
+            ? (
+              <Box
+                sx={{
+                  height: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Button
+                  startIcon={<AddCardIcon/>}
+                  onClick={toggleOpenNewCardForm}
+                > Add new card </Button>
+                <Tooltip title="Drag to move">
+                  <DragHandleIcon sx={{ cursor: 'pointer' }} />
+                </Tooltip>
+              </Box>
+            )
+            : (
+              <Box
+                sx={{
+                  height: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: 1
+                }}
+              >
+                <TextField
+                  label="Enter card title..."
+                  type="text"
+                  size='small'
+                  variant='outlined'
+                  autoFocus
+                  value={newCardTitle}
+                  onChange={(e) => {
+                    setNewCardTitle(e.target.value)
+                  }}
+                  sx={{
+                    '& label': { color: 'text.primary' },
+                    '& input': {
+                      color: (theme) => theme.palette.primary.main,
+                      bgColor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : 'white')
+                    },
+                    '& label.Mui-focused': { color: (theme) => theme.palette.primary.main },
+                    '& .MuiOutlinedInput-root': {
+                      '& fieldset': { borderColor: (theme) => theme.palette.primary.main },
+                      '&:hover fieldset': { borderColor: (theme) => theme.palette.primary.main },
+                      '&.Mui-focused fieldset': { borderColor: (theme) => theme.palette.primary.main }
+                    },
+                    '& .MuiOutlinedInput-input': {
+                      borderRadius: 1
+                    }
+                  }}
+                />
+
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: 1
+                  }}
+                >
+                  <Button
+                    onClick={addNewCard}
+                    variant='contained'
+                    color='success'
+                    size='small'
+                    sx={{
+                      boxShadow: 'none',
+                      border: '0.5px solid',
+                      borderColor: (theme) => theme.palette.success.main,
+                      '&:hover': { bgColor: (theme) => theme.palette.success.main }
+                    }}
+                  >
+                  Add
+                  </Button>
+                  <CloseIcon
+                    fontSize='small'
+                    sx={{
+                      color: (theme) => theme.palette.warning.light,
+                      cursor: 'pointer'
+                    }}
+                    onClick={toggleOpenNewCardForm}
+                  />
+                </Box>
+              </Box>
+            )
+          }
+
         </Box>
       </Box>
     </div>
--- trello-client/src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ trello-client/src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -4,8 +4,30 @@ import Button from '@mui/material/Button'
 import AddBoxIcon from '@mui/icons-material/AddBox'
 
 import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
+import { useState } from 'react'
+import { TextField } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+
 
 function ListColumns({ columns }) {
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false)
+  const toggleOpenNewColumnForm = () => setOpenNewColumnForm(!openNewColumnForm)
+
+  const [newColumnTitle, setNewColumnTitle] = useState('')
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      console.error('Please enter Column Title!')
+      return
+    }
+    console.log(newColumnTitle)
+
+    // Gọi API ở đây...
+
+    // Đóng trạng thái thêm Column mới và Clear Input
+    toggleOpenNewColumnForm()
+    setNewColumnTitle('')
+  }
+
   /**
    * --> SortableContext yêu cầu items là một mảng dạng ['id-1', 'id-2', 'id-3', ...]
    * CHỨ KHÔNG PHẢI [{id: 'id-1'}, {id: 'id-2'}, {id: 'id-3'}, ...]
@@ -26,27 +48,102 @@ function ListColumns({ columns }) {
         {columns?.map(column => <Column key={column._id} column={ column }/>)}
 
         {/* Box add new column CTA */}
-        <Box sx={{
-          minWidth: '200px',
-          maxWidth: '200px',
-          mx: 2,
-          borderRadius: '6px',
-          height: 'fit-content',
-          bgcolor: '#ffffff3d'
-        }}>
-          <Button
-            startIcon={<AddBoxIcon />}
-            sx={{
-              color: 'white',
-              width: '100%',
-              justifyContent: 'flex-start',
-              pl: 2.5,
-              py: 1
-            }}
-          >
+        {!openNewColumnForm
+          ? (
+            <Box
+              onClick={toggleOpenNewColumnForm}
+              sx={{
+                minWidth: '250px',
+                maxWidth: '250px',
+                mx: 2,
+                borderRadius: '6px',
+                height: 'fit-content',
+                bgcolor: '#ffffff3d'
+              }}>
+              <Button
+                startIcon={<AddBoxIcon />}
+                sx={{
+                  color: 'white',
+                  width: '100%',
+                  justifyContent: 'flex-start',
+                  pl: 2.5,
+                  py: 1
+                }}
+              >
             Add new column
-          </Button>
-        </Box>
+              </Button>
+            </Box>
+          )
+          : (
+            <Box sx={{
+              minWidth: '250px',
+              maxWidth: '250px',
+              mx: 2,
+              p: 1,
+              borderRadius: '6px',
+              height: 'fit-content',
+              bgcolor: 'ffffff3d',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1
+            }}>
+              <TextField
+                label="Enter column title..."
+                type="text"
+                size='small'
+                variant='outlined'
+                autoFocus
+                value={newColumnTitle}
+                onChange={(e) => {
+                  setNewColumnTitle(e.target.value)
+                }}
+                sx={{
+                  '& label': { color: 'white' },
+                  '& input': { color: 'white' },
+                  '& label.Mui-focused': { color: 'white' },
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': { borderColor: 'white' },
+                    '&:hover fieldset': { borderColor: 'white' },
+                    '&.Mui-focused fieldset': { borderColor: 'white' }
+                  }
+                }}
+              />
+
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: 1
+                }}
+              >
+                <Button
+                  onClick={addNewColumn}
+                  variant='contained'
+                  color='success'
+                  size='small'
+                  sx={{
+                    boxShadow: 'none',
+                    border: '0.5px solid',
+                    borderColor: (theme) => theme.palette.success.main,
+                    '&:hover': { bgColor: (theme) => theme.palette.success.main }
+                  }}
+                >
+                  Add Column
+                </Button>
+                <CloseIcon
+                  fontSize='small'
+                  sx={{
+                    color: 'white',
+                    cursor: 'pointer',
+                    '&:hover': { bgColor: (theme) => theme.palette.success.light }
+                  }}
+                  onClick={toggleOpenNewColumnForm}
+                />
+              </Box>
+            </Box>
+          )
+        }
+
       </Box>
     </SortableContext>
   )
